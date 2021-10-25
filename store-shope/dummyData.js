91c9fbeeabef2e6faec67ce98d0ef0b8fbe95956
marwaZhr <zahar.marwa.13@gmail.com>
2021-10-25 18:11:08 +0200

    product for home page

---
+++ store-shope/dummyData.js
@@ -0,0 +1,147 @@
+// liste of product
+
+export const  product = [
+    {   id:1,
+        image: 'https://contents.mediadecathlon.com/p1691845/k$ea7ac2b43e3d08cb55754cd7a6d353aa/sous-vetement-de-ski-femme-100-haut-noir.jpg?&f=452x452',
+        nom_du_produit: "Sous-vêtement de ski/ femme",
+        description: 'couleur : noir, taille : s',
+        prix: 5,
+        categorie : "femme"
+    },
+    {   id:2,
+        image: 'https://i.pinimg.com/236x/46/f9/f6/46f9f6a13227253b8535e6c6c888823f.jpg',
+        nom_du_produit: 'Sweet',
+        description: 'Sweat zippé london blanc femme/taille M',
+        prix: 25,
+        categorie : "femme"
+
+    },
+    {   id:3,
+        image: 'https://i.pinimg.com/originals/7d/9b/b6/7d9bb6b39a7e8e9f6adf2abcf4dc3e44.png',
+        nom_du_produit: 'collection',
+        description: 'Quality silverware, this 40 piece flatware set is made of premium high quality stainless steel which ensures your health, rust resistant, sturdy and durable for years to come',
+        prix: 35,
+        categorie : "femme"
+
+    },
+    {   id:4,
+        image: 'https://i.pinimg.com/236x/30/d1/f9/30d1f99bfced5c8bf43882f35c8b6863.jpg',
+        nom_du_produit: 'Robe',
+        description: 'It"s just a dummy data I do"t have to write description',
+        prix: 55,
+        categorie : "femme"
+
+    },
+    {   id:5,
+        image: 'https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/3d0133cc40a74fd8824ead12014ca896_9366/pantalon-de-survetement-adicolor-classics-primeblue-sst.jpg',
+        nom_du_produit: 'pantalon de survetement',
+        description: 'pantalon-de-survetement-adicolor-classics-primeblue',
+        prix: 95,
+        categorie : "homme"
+
+    },
+    {   id:6,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61U+gLIJLtL._AC._SR360,460.jpg',
+        nom_du_produit: 'Slip',
+        description: 'Jamais utilisés, believe me',
+        prix: 53,
+        categorie : "homme"
+
+    },
+    {   id:7,
+        image: 'https://image1.lacoste.com/dw/image/v2/AAQM_PRD/on/demandware.static/Sites-INT-Site/Sites-master/fr/dw2ebbb323/RC4028_B88_24.jpg?imwidth=375&impolicy=product',
+        nom_du_produit: 'Veste',
+        description: 'Utilisée deux fois, de taille L',
+        prix: 59,
+        categorie : "mixte"
+
+    },
+    {   id:8,
+        image: 'https://www.demain.fr/wp-content/uploads/2018/03/vetement.jpg',
+        nom_du_produit: 'Chemise',
+        description: 'Quelles que soient les circonstances, PINEL vous présente un choix très important de modèles et de marques Tout pour satisfaire votre goût et convenir à votre morphologie..',
+        prix: 5,
+        categorie : "homme"
+
+    },
+    {   id:9,
+        image: 'https://media.gqmagazine.fr/photos/5efca31383548f5bbff4e9b2/2:3/w_749,h_1124,c_limit/fd56bb5aad00fa3e761165535edd48ac.jpg',
+        nom_du_produit: 'Robe avec ruchés et nœuds',
+        description: 'Couleur:	Bordeaux, Style:Bohème, Type de motif:	imprimé floral, Longueur:	Court',
+        prix: 115,
+        categorie : "femme"
+
+    },
+    {
+        image: 'https://i.pmdstatic.net/fac/2021/03/19/b5d272bc-0b54-4e59-a8cf-41102467b329.jpeg',
+        nom_du_produit: 'Sac ',
+        description: 'sac sac sac sac',
+        prix: 10.5,
+        categorie : "autre"
+
+    },
+    {   id:10,
+        image: 'https://photos6.spartoo.com/photos/187/18708055/18708055_500_A.jpg',
+        nom_du_produit: 'Ceinture',
+        description: 'It don"t need it anymore, buy it',
+        prix: 5,
+        categorie : "mixte"
+
+    },
+    {   id:11,
+        image: 'https://photos6.spartoo.com/photos/815/8151802/8151802_350_A.jpg',
+        nom_du_produit: 'Converse',
+        description: 'It"s a converse you know bro right ?',
+        prix: 5,
+        categorie : "mixte"
+
+    },
+    {   id:12,
+        image: 'https://media.vogue.fr/photos/602556658272ea6ccc7202b4/master/w_1600%2Cc_limit/08_Neon_pink_bag_in_denim_AS2537_B05615_NC517.jpg',
+        nom_du_produit: 'Sac',
+        description: 'No description',
+        prix: 5,
+        categorie : "femme"
+
+    },
+    {   id:13,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61U+gLIJLtL._AC._SR360,460.jpg',
+        nom_du_produit: 'Slip',
+        description: 'Jamais utilisés, believe me',
+        prix: 53,
+        categorie : "homme"
+
+    },
+    {   id:14,
+        image: 'https://image1.lacoste.com/dw/image/v2/AAQM_PRD/on/demandware.static/Sites-INT-Site/Sites-master/fr/dw2ebbb323/RC4028_B88_24.jpg?imwidth=375&impolicy=product',
+        nom_du_produit: 'Veste',
+        description: 'Utilisée deux fois, de taille L',
+        prix: 59,
+        categorie : "mixte"
+
+    },
+    {   id:15,
+        image: 'https://www.demain.fr/wp-content/uploads/2018/03/vetement.jpg',
+        nom_du_produit: 'Chemise',
+        description: 'Quelles que soient les circonstances, PINEL vous présente un choix très important de modèles et de marques Tout pour satisfaire votre goût et convenir à votre morphologie..',
+        prix: 5,
+        categorie : "homme"
+
+    },
+    {   id:16,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61U+gLIJLtL._AC._SR360,460.jpg',
+        nom_du_produit: 'Slip',
+        description: 'Jamais utilisés, believe me',
+        prix: 53,
+        categorie : "homme"
+
+    },
+    {   id:17,
+        image: 'https://image1.lacoste.com/dw/image/v2/AAQM_PRD/on/demandware.static/Sites-INT-Site/Sites-master/fr/dw2ebbb323/RC4028_B88_24.jpg?imwidth=375&impolicy=product',
+        nom_du_produit: 'Veste',
+        description: 'Utilisée deux fois, de taille L',
+        prix: 59,
+        categorie : "mixte"
+
+    }
+]
