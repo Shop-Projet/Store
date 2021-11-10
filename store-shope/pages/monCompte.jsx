f4709ccf667fb6c08928afbc434307fed6f09de0
marwaZhr <zahar.marwa.13@gmail.com>
2021-11-10 19:55:57 +0100

    fix

--- store-shope/pages/monCompte.jsx
+++ store-shope/pages/monCompte.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
 import { Tabs, TabList, TabPanel, Tab } from "react-tabs";
 import ProductAccount from "../componenets/parts/dashboard/productAccount";
