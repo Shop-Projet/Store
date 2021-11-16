3df932c99d8eae1f6d77524c023bd7b1e87c5b6a
houssembouraoui <houssembouraoui6@gmail.com>
2021-11-16 17:38:26 +0100

    tryal 120325

--- store-shope/pages/monCompte.jsx
+++ store-shope/pages/monCompte.jsx
@@ -8,6 +8,7 @@ import { achat, utilisateur, vente } from "../dummyData";
 import { thePrice } from "../utils";
 import Image_profile from "../components/features/dashboard/image";
 import NbrDenfants from "../components/parts/dashboard/nbrDenfants";
+// comment 
 
 function DashBoard() {
   const [mesAchat, setAchat] = useState(achat);
